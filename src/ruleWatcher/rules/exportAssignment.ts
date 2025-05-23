00b6265d12f95a96594b5ac2a62bb7d3af1d1cf3
Aleksandar Ivanov <metalcho17@gmail.com>
2025-05-23 14:03:10 +0200

    export assignment done

--- src/ruleWatcher/rules/exportAssignment.ts
+++ src/ruleWatcher/rules/exportAssignment.ts
@@ -1,14 +1,21 @@
 import { ExportAssignment } from "../../model";
-import { BreakingChange } from "../../model/bcs";
+import { BC, BreakingChange } from "../../model/bcs";
+import { BCCreateType } from "../utils";
 
 export const checkExportAssignmentRules = (
   v1Decl: ExportAssignment,
   v2Decl: ExportAssignment,
-  BCCreate: (description: string) => BreakingChange,
+  BCCreate: BCCreateType,
 ): BreakingChange[] => {
   const breakingChanges: BreakingChange[] = [];
 
   console.log("Export Assignment rules are being checked", v1Decl.value);
 
+  // Check if the value changed - this is definitely not perfect but it will do the trick
+  if (v1Decl.value !== v2Decl.value) {
+    breakingChanges.push(
+      BCCreate(BC.exportAssignment.valueChanged(v1Decl.value, v2Decl.value)),
+    );
+  }
   return breakingChanges;
 };
